254579d47f481857baac8a9271b900361fc114bf
Chris Thompson <chris_thompson@bcit.ca>
2024-01-03 13:39:56 -0800

    Complete thread introduction code

--- src/concurrency/threadInheritance/ExtendingThread.java
+++ src/concurrency/threadInheritance/ExtendingThread.java
@@ -1,7 +1,10 @@
 package concurrency.threadInheritance;
 
 /**
- * We can extend the Thread class and override the run() method.
+ * We can extend the Thread class and override the run() method. In our previous example, the Thread constructor
+ * requires a Runnable object. But did you notice that Thread implements Runnable? That means we can extend Thread
+ * and override the run() method. This is a preferred way to create a Thread object for the JVM.
+ *
  *
  * @author BCIT
  * @version 2024
@@ -15,6 +18,14 @@ public final class ExtendingThread {
      * @param args unused
      */
     public static void main(final String[] args) {
+        Thread thread = new NewThread();
+        thread.start();
+    }
 
+    private static final class NewThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from " + this.getName());
+        }
     }
 }
---
+++ src/concurrency/threadInheritance/README.md
@@ -0,0 +1,7 @@
+## Welcome to concurrency!
+
+In this package, the source files introduce:
+
+1. how to instantiate threads with the Thread class directly
+2. how to extend the Thread class
+3. how threads interact (our first threaded program!)
