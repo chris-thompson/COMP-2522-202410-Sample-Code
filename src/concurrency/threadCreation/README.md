6ac0faf941541a52c4b1831e4c064484195e8900
Chris Thompson <chris_thompson@bcit.ca>
2024-01-03 12:19:48 -0800

    Hello world!

---
+++ src/concurrency/threadCreation/README.md
@@ -0,0 +1,7 @@
+## Welcome to concurrency!
+
+In this package, the source files introduce:
+
+1. how to instantiate threads with the Thread class and Runnable interface
+2. how to debug threads
+3. how to use some helpful methods in the Thread class.
---
+++ src/concurrency/threadInheritance/ExtendingThread.java
@@ -0,0 +1,20 @@
+package concurrency.threadInheritance;
+
+/**
+ * We can extend the Thread class and override the run() method.
+ *
+ * @author BCIT
+ * @version 2024
+ */
+public final class ExtendingThread {
+
+    private ExtendingThread() { }
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+
+    }
+}
