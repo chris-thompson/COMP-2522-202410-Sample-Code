ca1126f301ab164ea061af95d826363b7b3c02ee
Chris Thompson <chris_thompson@bcit.ca>
2024-01-04 22:18:32 -0800

    Complete join examples

--- src/concurrency/threadCoordination/JoiningThreads.java
+++ src/concurrency/threadCoordination/JoiningThreads.java
@@ -1,6 +1,9 @@
 package concurrency.threadCoordination;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>We can use the join() method to wait for a thread to finish. That is, it waits for the thread's run method
@@ -30,6 +33,36 @@ public final class JoiningThreads {
         // What about your laptop?
         int cores = Runtime.getRuntime().availableProcessors();
         System.out.println(cores);
+
+        List<Integer> inputNumbers = Arrays.asList(1, 10, 100, 1000, 10000, 100000, 1000000);
+        List<FactorialThread> threads = new ArrayList<>();
+
+        for (long inputNumber : inputNumbers) {
+            threads.add(new FactorialThread(inputNumber));
+        }
+
+        for (Thread thread : threads) {
+            // thread.setDaemon(true); // How does this change the performance of the program?
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            final int oneSecond = 1000;
+            try {
+                thread.join(oneSecond); // What happens if we change this to a longer time? Or remove the parameter?
+            } catch (InterruptedException e) {
+                System.out.println("Can you tell me when this line would be printed?");
+            }
+        }
+
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            FactorialThread factorialThread = threads.get(i);
+            if (factorialThread.isFinished()) {
+                System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
+            } else {
+                System.out.println("The calculation for " + inputNumbers.get(i) + " is still in progress");
+            }
+        }
     }
 
     private static final class FactorialThread extends Thread {
