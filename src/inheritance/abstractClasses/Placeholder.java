4ae5cec5dfb750b2cf9582e1b91bc62249c8d5d7
Chris Thompson <chris_thompson@bcit.ca>
2024-01-31 19:20:47 -0800

    Add the first inheritance hierarchy and placeholders for abstraction and order of initialization packages

---
+++ src/inheritance/abstractClasses/Placeholder.java
@@ -0,0 +1,19 @@
+package inheritance.abstractClasses;
+
+/**
+ * Placeholder.
+ *
+ * @author BCIT
+ * @version 2024
+ */
+public final class Placeholder {
+    private Placeholder() {
+    }
+    /**
+     * Drives the program.
+     * @param args (unused)
+     */
+    public static void main(final String[] args) {
+        System.out.println("Placeholder");
+    }
+}
